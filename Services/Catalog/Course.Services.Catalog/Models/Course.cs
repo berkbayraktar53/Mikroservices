e1274e3742f4bc40cc0675cf9aa70e199b6b2884
Kerem Berk Bayraktar <keremberk53@gmail.com>
2022-08-20 16:02:00 +0300

    Initial Create

---
+++ Services/Catalog/Course.Services.Catalog/Models/Course.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Course.Services.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] //Bu tip MongoDB'ye hangi özellikte olacağını belirtmek için verildi
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+        public Feature Feature { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        [BsonIgnore] //Üstteki satırları MongoDB'ye yansıtırken alltakini gözardı et anlamına geliyor
+        public Category Category { get; set; }
+    }
+}
---
+++ Shared/Course.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Course.Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; private set; }
+
+        [JsonIgnore] //Bu property'nin response'nin içinde olmasına gerek yok anlamına geliyor
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSucessFul { get; private set; }
+        public List<string> Errors { get; set; }
+
+        public static ResponseDto<T> Success(T data, int statusCode)
+        {
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode, IsSucessFul = true };
+        }
+
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> { Data = default(T), StatusCode = statusCode, IsSucessFul = true };
+        }
+
+        public static ResponseDto<T> Fail(List<string> errors, int statusCode)
+        {
+            return new ResponseDto<T> { Errors = errors, StatusCode = statusCode, IsSucessFul = false };
+        }
+
+        public static ResponseDto<T> Fail(string error, int statusCode)
+        {
+            return new ResponseDto<T> { Errors = new List<string>() { error }, StatusCode = statusCode, IsSucessFul = false };
+        }
+    }
+}
